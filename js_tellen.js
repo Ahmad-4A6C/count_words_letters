97eab11f479054fbb1f2a9030dfe4fe9da0b7a17
Ahmad-4A6C <505263@edu.rocmn.nl>
2017-06-16 09:30:52 +0200

    langste woord toegevoegd

--- js_tellen.js
+++ js_tellen.js
@@ -3,14 +3,28 @@ document.getElementById("woorden").addEventListener("keyup", tellen);
 function tellen() {
 	
 	var ingevoerdeTekst = document.getElementById("woorden").value;
+	var letters = ingevoerdeTekst.split("");
+	var woorden = ingevoerdeTekst.split(" ");
+	
+	var lgth = 0;
+	var longest;
+	
+	for(var i=0; i < woorden.length; i++){
+		if(woorden[i].length > lgth){
+			var lgth = woorden[i].length;
+			longest = woorden[i];
+		}
+    }
 	
 	document.getElementById("resultaat").innerHTML = "Teskt is: "+ ingevoerdeTekst;
+	document.getElementById("resultaat").innerHTML += "<br> Aantal woorden: " + woorden.length;
+	document.getElementById("resultaat").innerHTML += "<br>Aantal letters: " + letters.length;
+	document.getElementById("resultaat").innerHTML += "<br> Langste woord: "+ longest;
 	
-	document.getElementById("resultaat").innerHTML += "<br>Aantal letters: " + ingevoerdeTekst.length;
-	
-	var space = ingevoerdeTekst.split(" ").length;
-	document.getElementById("resultaat").innerHTML += "<br> Aantal woorden: " + space;
 }
+
+
+	
 function toUpperCase() {
  	var ingevoerdeTekst = document.getElementById("woorden").value;
 
